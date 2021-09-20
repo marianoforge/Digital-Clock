9b79a92cc46c1af96c6cae93a2908d08819b9312
Mariano De Simone <marianods@MSI-MDS.localdomain>
2021-09-19 21:16:51 -0300

    Digital Clock

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ const clock = document.querySelector('.clock');
 const tick = () => {
   const now = new Date();
 
-  const h = now.getHours();
-
+  const h = now.getHours() < 10 ? '0' + now.getHours() : now.getHours();
   const m = now.getMinutes() < 10 ? '0' + now.getMinutes() : now.getMinutes();
-
   const s = now.getSeconds() < 10 ? '0' + now.getSeconds() : now.getSeconds();
-
 
   const html = `<span>${h}</span> : <span>${m}</span> : <span>${s}</span>`;
 
